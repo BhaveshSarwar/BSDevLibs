58efb53455998105d561bec89e3d33522051e72a
Bhavesh Sarwar <bhavesh7sarwar@gmail.com>
2020-02-26 22:41:59 +0530

    Initial Commit

---
+++ BSDevLibs/BSDevLibs.h
@@ -0,0 +1,19 @@
+//
+//  BSDevLibs.h
+//  BSDevLibs
+//
+//  Created by Bhavesh Sarwar on 26/02/20.
+//  Copyright © 2020 Bhavesh Sarwar. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for BSDevLibs.
+FOUNDATION_EXPORT double BSDevLibsVersionNumber;
+
+//! Project version string for BSDevLibs.
+FOUNDATION_EXPORT const unsigned char BSDevLibsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BSDevLibs/PublicHeader.h>
+
+
